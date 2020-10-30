86c1ed354a376b909b0727bb87a559b20596e00b
Vijay Patel <vpatel15@slb.com>
2020-10-30 16:36:18 +0530

    fix: 🐛 Fixed icons for header module

--- my-app/src/app/app-shared/app-shared.module.ts
+++ my-app/src/app/app-shared/app-shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { LogoComponent } from './logo/logo.component';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
+
 @NgModule({
   declarations: [LogoComponent, HeaderComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [BrowserModule, NgxBootstrapIconsModule.pick(allIcons)],
   exports: [LogoComponent, HeaderComponent, FooterComponent],
 })
 export class AppSharedModule {}
